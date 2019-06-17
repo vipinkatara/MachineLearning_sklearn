7e1cf24e6639ed435e201add1f6562d30a099f9c
Vipin Katara <vipinkatara2@gmail.com>
2019-06-18 02:51:08 +0530

    Initial Commit

---
+++ randomforest.py
@@ -0,0 +1,85 @@
+# Decision Tree with Gini IndexPython
+# clf_gini = DecisionTreeClassifier(criterion = "gini", random_state = 100,
+#                                max_depth=3, min_samples_leaf=5)
+# clf_gini.fit(X_train, y_train)
+
+
+import math
+
+import numpy as np
+
+import pandas as pd
+
+#from pandas import DataFrame
+
+from sklearn import preprocessing
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+#from sklearn.cross_validation import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+from numpy import loadtxt, where
+
+from pylab import scatter, show, legend, xlabel, ylabel
+
+# scale larger positive and values to between -1,1 depending on the largest
+# value in the data
+
+min_max_scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
+
+df = pd.read_csv("train.csv", header=0)
+
+# clean up data
+
+df.columns = ['PassengerId',	'Survived',	'Name',	'Sex',	'Age',	'SibSp',	'Parch',	'Ticket',	'Fare',	'Cabin',	'Embarked',	'Pclass']
+df = df[["Sex", "Age", "Survived", "Pclass", "Parch","Fare"]]
+cd = df['Pclass']
+df.dropna(inplace=True)
+print(cd.head)
+#x = df["Survived"]
+
+X = df[["Pclass", "Sex", "Age","Fare"]]
+
+X = np.array(X)
+
+X = min_max_scaler.fit_transform(X)
+
+Y = df["Survived"]
+
+Y = np.array(Y)
+X = X.astype(float)
+Y = Y.astype(float)
+
+X_train, X1_test, Y_train, Y1_test = train_test_split(X, Y,test_size=0.0001)
+
+# train scikit learn model
+df = pd.read_csv("test.csv", header=0)
+
+# clean up data
+
+df.columns = ["Sex","Age","Survived","Pclass", "Cabin","PassengerId","Name", "SibSp", "Parch", "Ticket", "Fare", "Embarked"]
+df.dropna(inplace=True)
+x = df["Survived"]
+
+X1 = df[["Pclass","Sex","Age","Fare"]]
+print(df.head)
+X1 = np.array(X)
+
+X1 = min_max_scaler.fit_transform(X)
+
+Y1 = df["Survived"]
+
+Y1 = np.array(Y)
+Y1 = Y1.astype(float)
+X1 = X1.astype(float)
+X1_train, X_test, Y1_train, Y_test = train_test_split(X1, Y1, test_size=0.99, random_state=101)
+
+clf = RandomForestClassifier()
+clf.fit(X_train, Y_train)
+
+
+accuracy = clf.score(X_test,Y_test)
+
+print('accuracy = ', accuracy)
